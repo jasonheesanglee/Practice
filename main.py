a5c4540629b427aa9c4cae5e3e4fc94860d8c3bc
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-02-27 11:01:48 +0900

    Adding up the lecture from Coursera

--- main.py
+++ main.py
@@ -1684,3 +1684,56 @@ Ken is 30 years old and works as Chef
 Pat is 35 years old and works as Lawyer
 Amanda is 25 years old and works as Engineer
 """)
+
+print()
+print("Dictionary")
+print("Key - Value : One Set")
+x = {}
+print(type(x))
+
+print()
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+print(file_counts)
+print(file_counts["txt"])
+print("jpg" in file_counts)
+print("html" in file_counts)
+
+print()
+file_counts["cfg"] = 8
+print(file_counts)
+file_counts["csv"] = 17
+print(file_counts)
+del file_counts["cfg"]
+print(file_counts)
+
+print()
+file_counts = {"jpg":10, "txt": 14, "csv": 2, "py": 23}
+for extension in file_counts:
+    print(extension)
+for ext, amount in file_counts.items():
+    print("There are {} files with the .{} extension".format(amount,ext))
+print(file_counts.keys())
+print(file_counts.values())
+print()
+for value in file_counts.values():
+    print(value)
+
+
+print()
+print("Practice Questions - Dictionary & Format")
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for animals, characteristics in cool_beasts.items():
+    print("{} have {}".format(animals, characteristics))
+
+print()
+def count_letters(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return print(result)
+count_letters("aaaaa")
+count_letters("tenant")
+count_letters("a long string with a lot of letters")
+
