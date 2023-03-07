d1a044923a77ddc0bdf4a4d25d799e58f208c91e
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-03-07 16:17:45 +0900

    Py4E

--- Py4E.py
+++ Py4E.py
@@ -421,5 +421,41 @@ computepay(fh, fr)
 print("Pay:", xp)
 
 print()
+print("Loops and Iteration")
 
+print()
 print("While Loop")
+
+print()
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("Blastoff!")
+print(n)
+
+print()
+n = 0
+while n > 0:
+    print("Lather")
+    print("Rinse")
+print("Dry off!")
+
+print()
+while True:
+    line = input("> ")
+    if line == "done":
+        break
+    print(line)
+print("Done!")
+
+print()
+while True:
+    line = input("> ")
+    if line[0] == "#":
+        continue
+    if line == "done":
+        break
+    print(line)
+print("Done!")
+
