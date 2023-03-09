06e76f7b38341b094f7ef0ad02fa40ce2e30f0ff
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-03-09 11:15:36 +0900

    Py4E

--- Py4E.py
+++ Py4E.py
@@ -986,3 +986,42 @@ for line in han:
     print(wds[2])
 
 print()
+
+print("Python Dictionary")
+
+print("List - in order\nDictionary - in a bag, not in order")
+
+print()
+purse = dict()
+purse["money"] = 12
+purse["candy"] = 3
+purse["tissues"] = 75
+print(purse)
+print(purse["candy"])
+purse["candy"] = purse["candy"]+2
+print(purse)
+
+print()
+lst = list()
+lst.append(21)
+lst.append(183)
+print(list)
+lst[0] = 23
+print(list)
+
+print()
+ddd = dict()
+ddd["age"] = 21
+ddd["course"] = 182
+print(ddd)
+ddd["age"] = 23
+print(ddd)
+
+print()
+jjj = {"chuck": 1, "fred": 42, "jan": 100}
+print(jjj)
+ooo = {}
+print(ooo)
+
+print()
+
