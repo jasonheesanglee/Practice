31875b9343047ce8910720b956097cdde8a05e71
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-03-09 16:41:32 +0900

    Py4E

--- Py4E.py
+++ Py4E.py
@@ -1311,3 +1311,75 @@ print()
 
 c = {"a": 10, "b": 1, "c": 22}
 print(sorted([(v, k) for k, v in c.items()]))
+
+print()
+
+print("Last Practice Exercise")
+print()
+fname = input("Enter File: ")
+if len(fname)<1 : fname = "clown.txt"
+hand = open(fname)
+
+di = dict()
+for lin in hand:
+    lin = lin.rstrip()
+    wds = lin.split()
+    for w in wds:
+        di[w] = di.get(w, 0) +1
+
+#  print(di)
+
+tmp = list()
+for k, v in di.items():
+    #  print(k, v)
+    newt = (v, k)
+    tmp.append(newt)
+#  print("Flipped", tmp)
+
+tmp = sorted(tmp, reverse=True)
+#  print("Sorted", tmp[:5])
+
+for v, k in tmp[:5]:
+    print(k,v)
+
+print()
+
+print(("kim", "park", "lee") < ("son", "jung", "min"))
+print(("A", "B", "C") < ("해", "달", "별"))
+print((0, 10, 100) < (0, 1, 1000))
+print(("가", "나", "다") < ("강", "날", "단"))
+print(("James", 22) < ("happy", "호수", "산", 1000))
+
+
+zen = '''
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
+
+di = dict()
+words = zen.split()
+for word in words:
+  di[word] = di.get(word,0) + 1
+
+word_list = sorted([(v,k) for k,v in di.items()], reverse = True)
+
+print('Done-->',word_list[0][1],word_list[0][0])
