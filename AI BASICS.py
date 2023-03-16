d47c7714cb54c2421f2b1703c592af905738eea0
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-03-16 19:29:10 +0900

    3/16 commit

--- AI BASICS.py
+++ AI BASICS.py
@@ -608,8 +608,16 @@ else:
 print()
 print("Are you here?")
 
+kor_score = [49, 79, 20, 100, 80]
+math_score = [43, 59, 85, 30, 90]
+eng_score = [49, 97, 48, 60, 100]
+midterm_score = [kor_score, math_score, eng_score]
+
+student_score = [0, 0, 0, 0, 0]
+i = 0
+
 for subject in midterm_score:
-    for score in subject:˚
+    for score in subject:
         student_score[i] += score
         i += 1
     i = 0
@@ -619,13 +627,22 @@ else:
     student_average_integer = list()
     a, b, c, d, e = student_score
     student_average = [a/3, b/3, c/3, d/3, e/3]
+
     for j in student_average:
         j = float(f"{j:.2f}")  # 여기까진 오케이
         student_average_two_decimal.append(j)
+
         for k in student_average_two_decimal:
             if k.is_integer() == True:
                 k = int(k)
             else:
-                k = k
-            student_average_integer.append(k)
-            print(student_average_integer)
+                continue
+        student_average_integer.append(k)
+    print(student_average_integer)
+
+print("Yay!!!!!!!!!!!!!!!!!!!!!! Please check this file to check my effort\nhttps://github.com/jasonheesanglee/Practice/blob/main/Took%20me%202%20hours%20to%20solve%20this")
+
+print()
+
+print("debugging")
+
