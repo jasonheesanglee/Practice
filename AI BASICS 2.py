03d8df122ef3577d003d83a6f1aee2862487984f
jasonheesanglee <123557477+jasonheesanglee@users.noreply.github.com>
2023-03-17 16:32:51 +0900

    3/17 commit

--- AI BASICS 2.py
+++ AI BASICS 2.py
@@ -8,4 +8,80 @@ print(a)
 c = a.pop()
 print(a)
 print(c)
+print()
 
+word = input("Input a word Stack: ")
+word_list = list(word)
+# print(word_list)
+for _ in range(len(word_list)):
+    print(word_list.pop())
+    print(word_list)
+
+print()
+a = [1,2,3,4,5]
+print(a)
+a.append(10)
+print(a)
+a.append(20)
+print(a)
+
+c = a.pop(0)
+print(a)
+print(c)
+
+word = input("Input a word Queue: ")
+word_list = list(word)
+# print(word_list)
+for _ in range(len(word_list)):
+    print(word_list.pop(0))
+    print(word_list)
+
+print()
+
+t = (1,2,3)
+print(type(t))
+print(t + t)
+print(t * 2)
+print(t)
+print(len(t))
+#  t[1] = 5
+#  print(t)
+
+t = (1)
+print(type(t))
+
+t = (1,)
+print(type(t))
+
+
+print()
+s = set([1,2,3,1,2,3])
+print(s)
+
+s.add(1)
+print(s)
+
+s.remove(1)
+print(s)
+
+s.update([1,4,5,6,7])
+print(s)
+
+s.discard(3)
+print(s)
+s.clear()
+print(s)
+
+print()
+
+s1 = set([1,2,3,4,5])
+s2 = set([3,4,5,6,7])
+
+print(s1.union(s2))
+
+print(s1 | s2)
+
+print(s1.intersection(s2))
+print(s1 & s2)
+print(s1.difference(s2))
+print(s1 - s2)
